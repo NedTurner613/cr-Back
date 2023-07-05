413dfe9629a0fcba7a591eae9c7018e7f7594419
Ned <nedturner613@gmail.com>
2023-07-05 12:46:27 -0500

    second commit

---
+++ src/main/java/com/calc/crBack/CrBackApplication.java
@@ -0,0 +1,13 @@
+package com.calc.crBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calc/crBack/CrBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calc.crBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
